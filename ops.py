eff8179cdccefa5628bf534e8feacdff600e6b86
IvanGuGon1 <certified.courses@outlook.es>
2022-10-08 00:46:39 +0200

    test

---
+++ ops.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    """
+    test1
+    """
+    print("hola mundo")
+    return x+y
+
+def subtract(x,y):
+    print("hola")
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
